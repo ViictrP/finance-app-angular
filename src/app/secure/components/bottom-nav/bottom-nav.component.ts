58355c79bad3fc29ec3be4c8688d5873d4b93edf
Victor <vtrownz@hotmail.com>
2022-09-11 11:51:54 -0300

    separing header and bottom-nav from secure component

---
+++ src/app/secure/components/bottom-nav/bottom-nav.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-nav',
+  templateUrl: './bottom-nav.component.html',
+  styleUrls: ['./bottom-nav.component.css']
+})
+export class BottomNavComponent {
+
+  constructor() {
+  }
+
+}
--- src/app/secure/components/secure.component.spec.ts
+++ src/app/secure/components/secure.component.spec.ts
@@ -1,31 +1,22 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SecureComponent} from './secure.component';
-import {LoginService} from '../../public/services/login.service';
-import {PublicModule} from '../../public/public.module';
-import {Router} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
 
 describe('SecuredComponent', () => {
   let component: SecureComponent;
   let fixture: ComponentFixture<SecureComponent>;
-  let loginService: LoginService;
-  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SecureComponent],
       imports: [
-        PublicModule,
         RouterTestingModule
-      ],
-      providers: [LoginService]
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SecureComponent);
     component = fixture.componentInstance;
-    loginService = TestBed.inject(LoginService);
-    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
@@ -33,12 +24,5 @@ describe('SecuredComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should log out with success', () => {
-    const logOutSpy = jest.spyOn(loginService, 'logOut');
-    const navigateSpy = jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
-    component.logOut();
 
-    expect(logOutSpy).toHaveBeenCalled();
-    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
-  });
 });
