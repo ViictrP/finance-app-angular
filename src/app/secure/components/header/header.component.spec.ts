98b434ff2ddefd28546ad2c7c9d56aa51ee5dd0c
Victor <vtrownz@hotmail.com>
2022-09-11 11:52:53 -0300

    separing header and bottom-nav from secure component

--- src/app/secure/components/header/header.component.spec.ts
+++ src/app/secure/components/header/header.component.spec.ts
@@ -3,6 +3,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeaderComponent} from './header.component';
 import {LoginService} from '../../../public/services/login.service';
 import {Router} from '@angular/router';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -13,6 +14,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
+      imports: [HttpClientTestingModule],
       providers: [LoginService]
     }).compileComponents();
 
