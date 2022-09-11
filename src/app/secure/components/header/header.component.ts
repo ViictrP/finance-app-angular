5411449cecebde3313596b58a060dc8660b7e963
Victor <victor.prado@capco.com>
2022-09-11 15:35:26 -0300

    fetching user profile from backend

--- src/app/secure/components/header/header.component.ts
+++ src/app/secure/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {LoginService} from '../../../public/services/login.service';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import User from '../../../entities/User';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +12,15 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent {
 
+  user$?: Observable<User>;
+
   constructor(private readonly loginService: LoginService,
+              private readonly userService: UserService,
               private readonly router: Router) {
   }
 
   ngOnInit(): void {
+    this.user$ = this.userService.getProfile();
   }
 
   logOut() {
