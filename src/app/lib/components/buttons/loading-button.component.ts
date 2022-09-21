d8d6d2fab7f1c45e9ee038e0a34c69b58ff16e9c
Victor <vtrownz@hotmail.com>
2022-09-21 19:33:02 -0300

    fixing some CSS styling issues

--- src/app/lib/components/buttons/loading-button.component.ts
+++ src/app/lib/components/buttons/loading-button.component.ts
@@ -5,7 +5,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   template: `
     <button
       [disabled]="disabled"
-      class="block w-full px-4 py-3 text-xl bg-blue-500 rounded-lg transition ease-in-out duration-100 hover:bg-sky-500 hover:border-sky-900 disabled:bg-blue-900 {{loading ? '' : 'disabled:text-gray-600'}} active:outline-pink-800 focus:outline-pink-800 focus:outline-8"
+      class="block w-full px-4 py-3 text-xl bg-blue-500 rounded-lg transition ease-in-out duration-100 hover:bg-sky-500 hover:border-sky-900 disabled:hover:bg-blue-900 disabled:bg-blue-900 {{loading ? '' : 'disabled:text-gray-600'}} focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-sky-500"
       (click)="onClick()"
       [type]="type">
       <div *ngIf="loading" class="flex justify-center gap-2 align-middle">
