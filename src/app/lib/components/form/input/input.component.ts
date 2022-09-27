2ae69b54b7c8a9f105f65f09da1711e8cb4deb2b
Victor <vtrownz@hotmail.com>
2022-09-26 22:56:45 -0300

    fix for focus border in app-input

--- src/app/lib/components/form/input/input.component.ts
+++ src/app/lib/components/form/input/input.component.ts
@@ -13,7 +13,7 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
         [type]="inputType"
         [placeholder]="placeholder"
         (input)="valueChanged()"
-        class="{{icon ? 'pl-10' : 'pl-5'}} pr-9 py-3 text-xl w-full rounded-md mb-1 bg-zinc-900 border-1 border-{{invalid && touched ? 'red-500' : 'transparent'}} focus:ring focus:ring-sky-500 transition ease-in-out duration-150"
+        class="{{icon ? 'pl-10' : 'pl-5'}} pr-9 py-3 text-xl w-full rounded-md mb-1 bg-zinc-900 border-1 focus:ring {{invalid && touched ? 'border-red-500 focus:ring-red-500' : 'focus:ring-sky-500 border-zinc-900'}} transition ease-in-out duration-150"
       />
       <i class="absolute top-[15px] left-2 text-2xl {{icon}}"></i>
       <button
