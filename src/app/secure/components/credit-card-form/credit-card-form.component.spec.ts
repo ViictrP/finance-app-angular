6784df09a221749a6d647cf43de29ff197f979cf
Victor <vtrownz@hotmail.com>
2022-10-06 14:59:46 -0300

    fixing unit tests

--- src/app/secure/components/credit-card-form/credit-card-form.component.spec.ts
+++ src/app/secure/components/credit-card-form/credit-card-form.component.spec.ts
@@ -1,3 +1,34 @@
-describe('AddCreditCard', () => {
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CreditCardFormComponent} from './credit-card-form.component';
+import {UserService} from '../../services/user.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormModule} from '../../../form.module';
+import {CreditCardService} from '../../services/credit-card.service';
 
+describe('AddCreditCard', () => {
+  let component: CreditCardFormComponent;
+  let fixture: ComponentFixture<CreditCardFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CreditCardFormComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        FormModule
+      ],
+      providers: [
+        UserService,
+        CreditCardService
+      ]
+    });
+
+    fixture = TestBed.createComponent(CreditCardFormComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- src/app/secure/components/transaction-form/transaction-form.component.spec.ts
+++ src/app/secure/components/transaction-form/transaction-form.component.spec.ts
@@ -1,3 +1,20 @@
-describe('TransactionForm', () => {
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TransactionFormComponent} from './transaction-form.component';
 
+describe('TransactionForm', () => {
+  let component: TransactionFormComponent;
+  let fixture: ComponentFixture<TransactionFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TransactionFormComponent]
+    });
+
+    fixture = TestBed.createComponent(TransactionFormComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
