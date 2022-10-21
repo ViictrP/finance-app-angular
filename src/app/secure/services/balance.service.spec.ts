c789a2bed2897a7cfe9df0d17006e56fb4cad17b
Victor <vtrownz@hotmail.com>
2022-10-21 15:45:08 -0300

    balance screen implemented

--- src/app/secure/services/balance.service.spec.ts
+++ src/app/secure/services/balance.service.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { BalanceService } from './balance.service';
+import {BalanceService} from './balance.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('BalanceService', () => {
   let service: BalanceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(BalanceService);
   });
 
