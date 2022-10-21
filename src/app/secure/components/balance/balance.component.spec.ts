8d63403ceeb0d5b119cd4d10e52e95463f06e080
Victor <vtrownz@hotmail.com>
2022-10-21 15:44:16 -0300

    fixing test

--- src/app/secure/components/balance/balance.component.spec.ts
+++ src/app/secure/components/balance/balance.component.spec.ts
@@ -1,6 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BalanceComponent } from './balance.component';
+import {BalanceComponent} from './balance.component';
+import {UserService} from '../../services/user.service';
+import {BalanceService} from '../../services/balance.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {InputDateComponent} from '../../../lib/components/form/input/input-date.component';
+import {CardComponent} from '../../../lib/components/card/card.component';
+import {InputComponent} from '../../../lib/components/form/input/input.component';
+import {FormModule} from '../../../form.module';
 
 describe('BalanceComponent', () => {
   let component: BalanceComponent;
@@ -8,9 +16,22 @@ describe('BalanceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BalanceComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        BalanceComponent,
+        InputDateComponent,
+        CardComponent,
+        InputComponent
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        FormModule
+      ],
+      providers: [
+        UserService,
+        BalanceService,
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BalanceComponent);
     component = fixture.componentInstance;
