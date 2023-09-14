4b0bb3536fca38f5597f03db89183b1a8b5b3da0
Victor <vtrownz@hotmail.com>
2023-09-14 11:02:45 -0300

    lazy loading

--- src/app/guards/guest.guard.spec.ts
+++ src/app/guards/guest.guard.spec.ts
@@ -1,48 +1,46 @@
 import {TestBed} from '@angular/core/testing';
 import {LoginService} from '../public/services/login.service';
 import {PublicModule} from '../public/public.module';
-import {GuestGuard} from './guest.guard';
 import {Router} from '@angular/router';
+import { guestGuard } from './guest.guard';
 
 describe('GuestGuard', () => {
-  let guard: GuestGuard;
   let loginService: LoginService;
   let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [PublicModule],
-      providers: [LoginService, GuestGuard]
+      providers: [LoginService]
     });
-    guard = TestBed.inject(GuestGuard);
     loginService = TestBed.inject(LoginService);
     router = TestBed.inject(Router);
   });
 
-  it('Should be created', () => {
-    expect(guard).toBeTruthy();
-  });
-
   it('Should return true if user is not logged in', async () => {
-    const promise = new Promise<boolean>((resolve) => {
-      resolve(false);
-    });
-    jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
-    const canActivate = await guard.canActivate({} as any, {} as any);
+    await TestBed.runInInjectionContext(async () => {
+      const promise = new Promise<boolean>((resolve) => {
+        resolve(false);
+      });
+      jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
+      const canActivate = await guestGuard();
 
-    expect(canActivate).toBeTruthy();
+      expect(canActivate).toBeTruthy();
+    });
   });
 
   it('Should return false and navigate to home if user is logged in', async () => {
-    const promise = new Promise<boolean>((resolve) => {
-      resolve(true);
-    });
-    jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
-    jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
-    const navigateSpy = jest.spyOn(router, 'navigate');
-    const canActivate = await guard.canActivate({} as any, {} as any);
+    await TestBed.runInInjectionContext(async () => {
+      const promise = new Promise<boolean>((resolve) => {
+        resolve(true);
+      });
+      jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
+      jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
+      const navigateSpy = jest.spyOn(router, 'navigate');
+      const canActivate = await guestGuard();
 
-    expect(canActivate).toBeFalsy();
-    expect(navigateSpy).toHaveBeenCalledWith(['/secure/home']);
+      expect(canActivate).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/secure/home']);
+    })
   });
 });
--- src/app/guards/logged-in.guard.spec.ts
+++ src/app/guards/logged-in.guard.spec.ts
@@ -1,44 +1,42 @@
-import {LoggedInGuard} from './logged-in.guard';
-import {LoginService} from '../public/services/login.service';
-import {Router} from '@angular/router';
-import {TestBed} from '@angular/core/testing';
-import {PublicModule} from '../public/public.module';
+import { LoginService } from '../public/services/login.service';
+import { Router } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+import { PublicModule } from '../public/public.module';
+import { loggedInGuard } from './logged-in.guard';
 
 describe('LoggedInGuard', () => {
-  let guard: LoggedInGuard;
   let loginService: LoginService;
   let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [PublicModule],
-      providers: [LoginService, LoggedInGuard]
+      providers: [LoginService]
     });
-    guard = TestBed.inject(LoggedInGuard);
     loginService = TestBed.inject(LoginService);
     router = TestBed.inject(Router);
   });
 
-  it('Should be created', () => {
-    expect(guard).toBeTruthy();
-  });
-
   it('Should return true if user is logged in', async () => {
-    const promise = new Promise<boolean>((resolve) => resolve(true));
-    jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
-    const canActivate = await guard.canActivate({} as any, {} as any);
+    await TestBed.runInInjectionContext(async () => {
+      const promise = new Promise<boolean>((resolve) => resolve(true));
+      jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
+      const canActivate = await loggedInGuard();
 
-    expect(canActivate).toBeTruthy();
+      expect(canActivate).toBeTruthy();
+    });
   });
 
   it('Should return false and navigate to login page if user is not logged in', async () => {
-    const promise = new Promise<boolean>((resolve) => resolve(false));
-    jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
-    jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
-    const navigateSpy = jest.spyOn(router, 'navigate');
-    const canActivate = await guard.canActivate({} as any, {} as any);
+    await TestBed.runInInjectionContext(async () => {
+      const promise = new Promise<boolean>((resolve) => resolve(false));
+      jest.spyOn(loginService, 'isLoggedIn').mockImplementation(() => promise);
+      jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
+      const navigateSpy = jest.spyOn(router, 'navigate');
+      const canActivate = await loggedInGuard();
 
-    expect(canActivate).toBeFalsy();
-    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+      expect(canActivate).toBeFalsy();
+      expect(navigateSpy).toHaveBeenCalledWith(['/public/login']);
+    })
   });
 });
