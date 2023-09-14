486d2b3690806f738cbe088ae2fc1d4e2874e1a0
Victor <vtrownz@hotmail.com>
2023-09-14 10:56:01 -0300

    lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {PlatformModule} from '@angular/cdk/platform';
-import {AppComponent} from './components/app.component';
-import {RoutingModule} from './routing.module';
-import {PublicModule} from './public/public.module';
-import {SecureModule} from './secure/secure.module';
-import {GuardsModule} from './guards/guards.module';
-import {registerLocaleData} from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PlatformModule } from '@angular/cdk/platform';
+import { AppComponent } from './components/app.component';
+import { RoutingModule } from './routing.module';
+import { PublicModule } from './public/public.module';
+import { SecureModule } from './secure/secure.module';
+import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
 import { LibModule } from './lib/lib.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +21,6 @@ registerLocaleData(ptBr);
     BrowserAnimationsModule,
     PlatformModule,
     RoutingModule,
-    GuardsModule,
     PublicModule,
     SecureModule,
     LibModule,
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,7 +1,6 @@
 import { AppComponent } from './components/app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { GuestGuard } from './guards/guest.guard';
-import { LoggedInGuard } from './guards/logged-in.guard';
+import { guestGuard, loggedInGuard } from './guards';
 import { NgModule } from '@angular/core';
 
 export const APP_ROUTES: Routes = [{
@@ -20,13 +19,20 @@ export const APP_ROUTES: Routes = [{
     },
     {
       path: 'public',
+      redirectTo: '/public/login',
+      pathMatch: 'full',
+    },
+    {
+      path: 'public',
       loadChildren: () => import('./public/public.module').then(m => m.PublicModule),
-      canActivate: [GuestGuard],
+      canActivateChild: [guestGuard],
+      canActivate: [guestGuard],
     },
     {
       path: 'secure',
       loadChildren: () => import('./secure/secure.module').then(m => m.SecureModule),
-      canActivate: [LoggedInGuard],
+      canActivateChild: [loggedInGuard],
+      canActivate: [loggedInGuard],
     },
   ],
 }];
