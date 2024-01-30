620791fc573eaec74a964fc82c8d93405c211a63
= <victor.prado@arc-one.com>
2024-01-30 20:44:36 -0300

    Code style setup

--- src/app/guards/public.guard.ts
+++ src/app/guards/public.guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "../services/auth.service";
 
-export const publicGuard: CanActivateFn = (route, state) => {
+export const publicGuard: CanActivateFn = () => {
   const isAuthenticated = inject(AuthService).isAuthenticated();
   const router = inject(Router);
   if (isAuthenticated) {
--- src/app/guards/secure.guard.ts
+++ src/app/guards/secure.guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "../services/auth.service";
 
-export const secureGuard: CanActivateFn = (route, state) => {
+export const secureGuard: CanActivateFn = () => {
   const isAuthenticated = inject(AuthService).isAuthenticated();
   const router = inject(Router);
   if (!isAuthenticated) {
