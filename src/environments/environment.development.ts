82ff9553d3b171f317fb3de3efcff498042b11d9
Victor <vtrownz@hotmail.com>
2024-02-19 22:20:19 -0300

    Reverting Docker changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.react.financeapp.dev',
   firebaseConfig: {
-    apiKey: 'AIzaSyBmvMmpS_VgSh0g6Sja4-2sTVzfDb50t4Q',
-    authDomain: 'finance-app-382403.firebaseapp.com',
-    projectId: 'finance-app-382403',
-    storageBucket: 'finance-app-382403.appspot.com',
-    messagingSenderId: '13736117814',
-    appId: '1:13736117814:web:3973b603e11fab03faba97',
-    measurementId: 'G-F2CSX9FKNR'
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId',
+    measurementId: 'measurementId'
   }
 };
