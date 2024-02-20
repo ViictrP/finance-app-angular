35c04aa224f2b0fd9074bd694c958c0534f0f6be
Victor <vtrownz@hotmail.com>
2024-02-19 23:28:19 -0300

    Implementing the home page

--- src/app/dto/profile.dto.ts
+++ src/app/dto/profile.dto.ts
@@ -1,7 +1,10 @@
 import CreditCardDTO from './credit-card.dto';
+import TransactionDTO from './transaction.dto';
 
 export default interface ProfileDTO {
   email: string;
   creditCards: CreditCardDTO[];
   salary: number;
+  transactions: TransactionDTO[];
+  recurringExpenses: TransactionDTO[];
 }
--- src/app/dto/transaction.dto.ts
+++ src/app/dto/transaction.dto.ts
@@ -1,4 +1,10 @@
 export default interface TransactionDTO {
   id: string;
   amount: number;
+  description: string;
+  category: string;
+  createdAt: Date;
+  deleteDate: Date;
+  deleted: boolean;
+  userId: string;
 }
